08015b2f9c54ec0f0c82417f428a5b00433fd666
Pihlaja <pihlaja@signaldust.com>
2021-02-01 18:24:38 +0200

    add basic tests (checks asserts only for now)

---
+++ run-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+BJIT=bin/bjit
+
+# This is random collection of very simple tests that are crafted
+# to expose potential problems mostly in CSE ruleset
+
+echo "Testing with output in 'test.out'"
+
+cat << END | $BJIT > test.out
+    x := 0/0; y := x/1u;
+    while(x < 10) { if(y != 2) x = x+1; x = x+1; } return x;
+END
+echo "Test pass"
+
+cat << END | $BJIT > test.out
+    x := 0/0; y := x/1u;
+    while(x < 10) { if(y != 2) x = x+1; x = x+1; } return y;
+END
+echo "Test pass"
+
+cat << END | $BJIT > test.out
+    x := 0/0; y := x/1u;
+    while(x < 10) { if(y != 2) y = x+1; x = x+1; } return x;
+END
+echo "Test pass"
+
+cat << END | $BJIT > test.out
+    x := 0/0; y := x/1u;
+    while(x < 10) { if(y != 2) x = x+1; else x = x+1; x=x+1; } return x;
+END
+echo "Test pass"
+
+cat << END | $BJIT > test.out
+    x := 0/0; y := x/1u;
+    while(x < 10) { if(y != 2) x = x+1; else x = x+1; } return x+1;
+END
+echo "Test pass"
