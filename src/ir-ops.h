d58f643a16e1d093e0e0d5b34d3340fa7c88eb1e
Pihlaja <pihlaja@signaldust.com>
2021-02-02 16:50:44 +0200

    tcallp is not SIDEFX if rets are not

--- src/ir-ops.h
+++ src/ir-ops.h
@@ -77,7 +77,7 @@
     _(fret, 0, 1), \
     _(iret, 0, 1), \
     _(iretI, 0, BJIT_IMM32), /* opt-dce needs to know which one is last */ \
-    _(tcallp, BJIT_SIDEFX, 1), \
+    _(tcallp, 0, 1), \
     /* */ \
     /* NOTE: THESE SHOULD MATCH THOSE STARTING FROM 'jilt' */ \
     /* SO MAKE SURE THE POSITIONS STAY RELATIVE */ \
