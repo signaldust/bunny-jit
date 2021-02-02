5ea88de55210fedd36d8c79ac970c400d1593a1c
Pihlaja <pihlaja@signaldust.com>
2021-02-02 03:32:13 +0200

    don't really need the echoes

--- run-tests.sh
+++ run-tests.sh
@@ -7,31 +7,26 @@ cat << END | bin/bjit
     x := 0/0; y := x/1u;
     while(x < 10) { if(y != 2) x = x+1; x = x+1; } return x;
 END
-echo "Test pass"
 
 cat << END | bin/bjit
     x := 0/0; y := x/1u;
     while(x < 10) { if(y != 2) x = x+1; x = x+1; } return y;
 END
-echo "Test pass"
 
 cat << END | bin/bjit
     x := 0/0; y := x/1u;
     while(x < 10) { if(y != 2) y = x+1; x = x+1; } return x;
 END
-echo "Test pass"
 
 cat << END | bin/bjit
     x := 0/0; y := x/1u;
     while(x < 10) { if(y != 2) x = x+1; else x = x+1; x=x+1; } return x;
 END
-echo "Test pass"
 
 cat << END | bin/bjit
     x := 0/0; y := x/1u;
     while(x < 10) { if(y != 2) x = x+1; else x = x+1; } return x+1;
 END
-echo "Test pass"
 
 cat << END | bin/bjit
     x := 0; y := 0/0; while(x < 10) { if(y != 2) x = x+1; else x = x+1; } return (x+1);
