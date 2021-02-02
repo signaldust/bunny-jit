5562f89eeff4574f06ec53ee2b6b7868e8930e51
Pihlaja <pihlaja@signaldust.com>
2021-02-02 13:52:25 +0200

    simple double/float convert test, fix fret

--- tests/test_add_ff.cpp
+++ tests/test_add_ff.cpp
@@ -5,21 +5,40 @@ int main()
 {
 
     bjit::Module    module;
-    bjit::Proc      proc(0, "dd");
 
-    proc.dret(proc.dadd(proc.env[0], proc.env[1]));
-
-    proc.opt();
+    {
+        bjit::Proc      proc(0, "ff");
+        proc.fret(proc.fadd(proc.env[0], proc.env[1]));
+        proc.opt();
+        module.compile(proc);
+    }
+    {
+        bjit::Proc      proc(0, "dd");
+        proc.dret(proc.dadd(proc.env[0], proc.env[1]));
+        proc.opt();
+        module.compile(proc);
+    }
+    {
+        bjit::Proc      proc(0, "dd");
+        proc.dret(proc.cf2d(proc.fadd(
+            proc.cd2f(proc.env[0]), proc.cd2f(proc.env[1]))));
+        proc.opt();
+        module.compile(proc);
+    }
     
-    int i = module.compile(proc);
+    auto & codeOut = module.getBytes();
+    
+    FILE * f = fopen("out.bin", "wb");
+    fwrite(codeOut.data(), 1, codeOut.size(), f);
+    fclose(f);
+    
+    printf(" - Wrote out.bin\n");
 
     assert(module.load());
-
-    auto ptr = module.getPointer<double(double,double)>(i);
-
-    printf(" 2 + 5 = %f\n", ptr(2, 5));
-
-    assert(ptr(2,5) == 7);
+    
+    assert(module.getPointer<float(float,float)>(0)(1.f, 5.5f) == 6.5f);
+    assert(module.getPointer<double(double,double)>(1)(2.5, 3.25) == 5.75);
+    assert(module.getPointer<double(double,double)>(2)(3.25, 4.5) == 7.75);
 
     return 0;
 }
