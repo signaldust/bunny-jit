4731e367a3b91a9ab58a5fe35651d6db9dad8181
Pihlaja <pihlaja@signaldust.com>
2021-02-03 17:07:57 +0200

    add missing emitters for fcalln/fcallp

--- tests/test_ci2f_cf2i.cpp
+++ tests/test_ci2f_cf2i.cpp
@@ -5,21 +5,37 @@ int main()
 {
 
     bjit::Module    module;
-    bjit::Proc      proc(0, "id");
 
-    proc.iret(proc.cd2i(proc.dadd(proc.ci2d(proc.env[0]), proc.env[1])));
+    {
+        bjit::Proc      proc(0, "if");
+        proc.iret(proc.cf2i(proc.fadd(proc.ci2f(proc.env[0]), proc.env[1])));
+        
+        proc.opt();
+        module.compile(proc);
+    }
 
-    proc.opt();
-    
-    int i = module.compile(proc);
+
+    {
+        bjit::Proc      proc(0, "id");
+        proc.iret(proc.cd2i(proc.dadd(proc.ci2d(proc.env[0]), proc.env[1])));
+        
+        proc.opt();
+        module.compile(proc);
+    }
+
 
     assert(module.load());
+    {
+        auto ptr = module.getPointer<int(int,float)>(0);
+        printf(" 2 + 5 = %d\n", ptr(2, 5));
+        assert(ptr(2,5) == 7);
+    }
 
-    auto ptr = module.getPointer<int(int,double)>(i);
-
-    printf(" 2 + 5 = %d\n", ptr(2, 5));
-
-    assert(ptr(2,5) == 7);
+    {
+        auto ptr = module.getPointer<int(int,double)>(1);
+        printf(" 2 + 5 = %d\n", ptr(2, 5));
+        assert(ptr(2,5) == 7);
+    }
 
     return 0;
 }
